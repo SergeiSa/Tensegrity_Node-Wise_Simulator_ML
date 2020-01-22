345bf3a7014bfbd29e9e5f057879c67d731377ad
SergeiSa <sergey89mtkgtu@mail.ru>
2020-01-22 18:35:02 +0300

    Added simulation capabilities

--- e_GenerateStructure_dynamics_speedtest.m
+++ e_GenerateStructure_dynamics_speedtest.m
@@ -29,23 +29,13 @@ robot.stiffness_coef = mu_cables + mu_rods;
 robot.nodes_position = [    0.8660   -0.8660    0         0.6411   -0.2979   -0.3139
                             0.5000    0.5000   -1.0000    0.0154    0.5613   -0.5585
                             0         0         0         1.0365    1.0277    1.0347];
-%%%%
-
-f_array = get_elastic_force_sums_nodes_wrapper1(robot)
-
-get_potential_energy_fnc_header = ...
-    get_potential_energy_fmincon_wrapper(robot.Connectivity, robot.nodes_position, ...
-                                         robot.stiffness_coef, robot.rest_lengths, [1; 2; 3]);
-x = fminunc(get_potential_energy_fnc_header, robot.nodes_position(:, 1:3));
-
-nodes_position = robot.nodes_position;
-nodes_position(:, 1:3) = x;
-
-f_array = get_elastic_force_sums_nodes(robot.Connectivity, nodes_position, ...
-                                         robot.stiffness_coef, robot.rest_lengths)
-
+robot.nodes_velocity = zeros(3, size(robot.Connectivity, 1));
+robot.nodes_masses = ones(size(robot.Connectivity, 1), 1);
+robot.nodes_dissipation = ones(size(robot.Connectivity, 1), 1);
 
 %%%%
-
-result = optimization_generate_rho_vector_and_function(Cables);
-
+tic;
+for i = 1:100
+    Res = Simulate(robot, 10, 10^(-3), [1, 2, 3]);
+end
+toc
